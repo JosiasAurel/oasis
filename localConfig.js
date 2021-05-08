17c494d98c6e3998096471167bac434b94fab7cf
JosiasAurel <ndjosiasaurel@gmail.com>
2021-05-08 01:24:28 +0100

    sync?

---
+++ localConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDJo96FycQz41duswtaT7hOJ_0ZZf6oj9k",
+    authDomain: "oasis-a4a40.firebaseapp.com",
+    projectId: "oasis-a4a40",
+    storageBucket: "oasis-a4a40.appspot.com",
+    messagingSenderId: "371502397936",
+    appId: "1:371502397936:web:76d24f8ff97dcad46e8535",
+    measurementId: "G-KBTHH5NNER"
+  };
